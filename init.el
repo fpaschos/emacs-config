7eedd325feff46e08a348b40ace4ad350bcf0233
Fotis Paschos <fpas@inaccess.com>
2015-01-15 12:41:51 +0200

    Changed frame navigation (windmove) shortcuts.

--- init.el
+++ init.el
@@ -150,10 +150,10 @@
 (when (fboundp 'windmove-default-keybindings)
   (windmove-default-keybindings))
 
-(global-set-key (kbd "C-c <left>")  'windmove-left)
-(global-set-key (kbd "C-c <right>") 'windmove-right)
-(global-set-key (kbd "C-c <up>")    'windmove-up)
-(global-set-key (kbd "C-c <down>")  'windmove-down)
+(global-set-key (kbd "C-x <left>")  'windmove-left)
+(global-set-key (kbd "C-x <right>") 'windmove-right)
+(global-set-key (kbd "C-x <up>")    'windmove-up)
+(global-set-key (kbd "C-x <down>")  'windmove-down)
 
 (defun ignore-error-wrapper (fn)
   "Funtion return new function that ignore errors.
